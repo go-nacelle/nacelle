600c88e0bba0c585a0dd34f075e2fb9666132799
Eric Fritz <eric@eric-fritz.com>
2017-08-12 10:27:19 -0500

    Add config implementation.

---
+++ process/config.go
@@ -0,0 +1,259 @@
+package process
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+	"reflect"
+	"strconv"
+	"strings"
+)
+
+type (
+	Config interface {
+		Load() []error
+		Register(key interface{}, config interface{}) error
+		MustRegister(key interface{}, config interface{})
+		Get(key interface{}) (interface{}, error)
+		MustGet(key interface{}) interface{}
+		ToMap() (map[string]interface{}, error)
+	}
+
+	PostLoadConfig interface {
+		PostLoad() error
+	}
+
+	EnvConfig struct {
+		prefix string
+		chunks map[interface{}]interface{}
+	}
+)
+
+const (
+	envTag      = "env"
+	maskTag     = "mask"
+	defaultTag  = "default"
+	requiredTag = "required"
+)
+
+var (
+	ErrDuplicateConfigKey = errors.New("duplicate config key")
+	ErrUnregisteredKey    = errors.New("no config registered to key")
+
+	replacer = strings.NewReplacer(
+		"\n", `\n`,
+		"\t", `\t`,
+		"\r", `\r`,
+	)
+)
+
+func NewEnvConfig(prefix string) Config {
+	return &EnvConfig{
+		prefix: prefix,
+		chunks: map[interface{}]interface{}{},
+	}
+}
+
+func (c *EnvConfig) Register(key interface{}, config interface{}) error {
+	if _, ok := c.chunks[key]; ok {
+		return ErrDuplicateConfigKey
+	}
+
+	c.chunks[key] = config
+	return nil
+}
+
+func (c *EnvConfig) MustRegister(key interface{}, config interface{}) {
+	if err := c.Register(key, config); err != nil {
+		panic(err.Error())
+	}
+}
+
+func (c *EnvConfig) Get(key interface{}) (interface{}, error) {
+	if config, ok := c.chunks[key]; ok {
+		return config, nil
+	}
+
+	return nil, ErrUnregisteredKey
+}
+
+func (c *EnvConfig) MustGet(key interface{}) interface{} {
+	config, err := c.Get(key)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return config
+}
+
+func (c *EnvConfig) Load() []error {
+	errors := []error{}
+
+	for _, chunk := range c.chunks {
+		errors = loadChunk(chunk, errors, c.prefix)
+	}
+
+	return errors
+}
+
+func (c *EnvConfig) ToMap() (map[string]interface{}, error) {
+	m := map[string]interface{}{}
+
+	for _, chunk := range c.chunks {
+		if err := dumpChunk(chunk, m); err != nil {
+			return nil, err
+		}
+	}
+
+	return m, nil
+}
+
+func loadChunk(obj interface{}, errors []error, prefix string) []error {
+	var (
+		ov = reflect.ValueOf(obj)
+		oi = reflect.Indirect(ov)
+		ot = oi.Type()
+	)
+
+	for i := 0; i < ot.NumField(); i++ {
+		var (
+			fieldType   = ot.Field(i)
+			fieldValue  = oi.Field(i)
+			envTag      = fieldType.Tag.Get(envTag)
+			defaultTag  = fieldType.Tag.Get(defaultTag)
+			requiredTag = fieldType.Tag.Get(requiredTag)
+		)
+
+		if envTag == "" {
+			continue
+		}
+
+		name := strings.ToUpper(fmt.Sprintf("%s_%s", prefix, envTag))
+
+		err := loadField(
+			fieldType,
+			fieldValue,
+			name,
+			defaultTag,
+			requiredTag,
+		)
+
+		if err != nil {
+			errors = append(errors, err)
+		}
+	}
+
+	if plc, ok := obj.(PostLoadConfig); ok {
+		if err := plc.PostLoad(); err != nil {
+			errors = append(errors, err)
+		}
+	}
+
+	return errors
+}
+
+func loadField(fieldType reflect.StructField, fieldValue reflect.Value, envTag, defaultTag, requiredTag string) error {
+	if !fieldValue.IsValid() {
+		return fmt.Errorf("field '%s' is invalid", fieldType.Name)
+	}
+
+	if !fieldValue.CanSet() {
+		return fmt.Errorf("field '%s' can not be set", fieldType.Name)
+	}
+
+	val, ok := os.LookupEnv(envTag)
+	if ok {
+		if !toJSON([]byte(val), fieldValue.Addr().Interface()) {
+			return fmt.Errorf("value supplied for field '%s' cannot be coerced into the expected type", fieldType.Name)
+		}
+
+		return nil
+	}
+
+	if requiredTag != "" {
+		val, err := strconv.ParseBool(requiredTag)
+		if err != nil {
+			return fmt.Errorf("field '%s' has an invalid required tag", fieldType.Name)
+		}
+
+		if val {
+			return fmt.Errorf("no value supplied for field '%s'", fieldType.Name)
+		}
+	}
+
+	if defaultTag != "" {
+		if !toJSON([]byte(defaultTag), fieldValue.Addr().Interface()) {
+			return fmt.Errorf("default value for field '%s' cannot be coerced into the expected type", fieldType.Name)
+		}
+
+		return nil
+	}
+
+	return nil
+}
+
+func toJSON(data []byte, v interface{}) bool {
+	if json.Unmarshal(data, v) == nil {
+		return true
+	}
+
+	ptr := reflect.ValueOf(v)
+
+	if ptr.Kind() == reflect.Ptr && reflect.Indirect(ptr).Kind() == reflect.String {
+		if json.Unmarshal(quoteJSON(data), v) == nil {
+			return true
+		}
+	}
+
+	return false
+}
+
+func quoteJSON(data []byte) []byte {
+	return []byte(fmt.Sprintf(`"%s"`, replacer.Replace(string(data))))
+}
+
+func dumpChunk(obj interface{}, m map[string]interface{}) error {
+	var (
+		ov = reflect.ValueOf(obj)
+		oi = reflect.Indirect(ov)
+		ot = oi.Type()
+	)
+
+	for i := 0; i < ot.NumField(); i++ {
+		var (
+			fieldType  = ot.Field(i)
+			fieldValue = oi.Field(i)
+			envTag     = fieldType.Tag.Get(envTag)
+			maskTag    = fieldType.Tag.Get(maskTag)
+		)
+
+		if envTag == "" {
+			continue
+		}
+
+		if maskTag != "" {
+			val, err := strconv.ParseBool(maskTag)
+			if err != nil {
+				return fmt.Errorf("field '%s' has an invalid mask tag", fieldType.Name)
+			}
+
+			if val {
+				continue
+			}
+		}
+
+		if fieldValue.Kind() == reflect.String {
+			m[fieldType.Name] = fmt.Sprintf("%s", fieldValue)
+		} else {
+			data, err := json.Marshal(fieldValue.Interface())
+			if err != nil {
+				return err
+			}
+
+			m[fieldType.Name] = string(data)
+		}
+	}
+
+	return nil
+}
---
+++ process/config_test.go
@@ -0,0 +1,307 @@
+package process
+
+import (
+	"errors"
+	"os"
+	"testing"
+
+	. "github.com/onsi/gomega"
+)
+
+type ConfigSuite struct{}
+
+func (s *ConfigSuite) SetUpTest(t *testing.T) {
+	os.Clearenv()
+}
+
+func (s *ConfigSuite) TestSimpleConfig(t *testing.T) {
+	var (
+		config = NewEnvConfig("app")
+		chunk  = &TestSimpleConfig{}
+	)
+
+	os.Setenv("APP_X", "foo")
+	os.Setenv("APP_Y", "123")
+	os.Setenv("APP_W", `["bar", "baz", "bonk"]`)
+
+	Expect(config.Register("simple", chunk)).To(BeNil())
+	Expect(config.Load()).To(BeEmpty())
+
+	loadedChunk, err := config.Get("simple")
+	Expect(err).To(BeNil())
+
+	Expect(loadedChunk).To(BeIdenticalTo(chunk))
+	Expect(chunk.X).To(Equal("foo"))
+	Expect(chunk.Y).To(Equal(123))
+	Expect(chunk.Z).To(Equal([]string{"bar", "baz", "bonk"}))
+}
+
+func (s *ConfigSuite) TestNestedJSONDeserialization(t *testing.T) {
+	var (
+		config = NewEnvConfig("app")
+		chunk  = &TestEmbeddedJSONConfig{}
+	)
+
+	os.Setenv("APP_P1", `{"v_int": 3, "v_float": 3.14, "v_bool": true}`)
+	os.Setenv("APP_P2", `{"v_int": 5, "v_float": 6.28, "v_bool": false}`)
+
+	Expect(config.Register("json-embedded", chunk)).To(BeNil())
+	Expect(config.Load()).To(BeEmpty())
+
+	loadedChunk, err := config.Get("json-embedded")
+	Expect(err).To(BeNil())
+
+	Expect(loadedChunk).To(BeIdenticalTo(chunk))
+	Expect(chunk.P1).To(Equal(&TestJSONPayload{V1: 3, V2: 3.14, V3: true}))
+	Expect(chunk.P2).To(Equal(&TestJSONPayload{V1: 5, V2: 6.28, V3: false}))
+}
+
+func (s *ConfigSuite) TestRequired(t *testing.T) {
+	var (
+		config = NewEnvConfig("app")
+		chunk  = &TestRequiredConfig{}
+	)
+
+	Expect(config.Register("required-config", chunk)).To(BeNil())
+
+	errors := config.Load()
+	Expect(errors).To(HaveLen(1))
+	Expect(errors).To(ContainElement(MatchError("no value supplied for field 'X'")))
+}
+
+func (s *ConfigSuite) TestDefault(t *testing.T) {
+	var (
+		config = NewEnvConfig("app")
+		chunk  = &TestDefaultConfig{}
+	)
+
+	Expect(config.Register("default-config", chunk)).To(BeNil())
+	Expect(config.Load()).To(BeEmpty())
+
+	loadedChunk, err := config.Get("default-config")
+	Expect(err).To(BeNil())
+
+	Expect(loadedChunk).To(BeIdenticalTo(chunk))
+	Expect(chunk.X).To(Equal("foo"))
+	Expect(chunk.Y).To(Equal([]string{"bar", "baz", "bonk"}))
+}
+
+func (s *ConfigSuite) TestBadType(t *testing.T) {
+	var (
+		config = NewEnvConfig("app")
+		chunk  = &TestSimpleConfig{}
+	)
+
+	os.Setenv("APP_X", "123") // silently converted to string
+	os.Setenv("APP_Y", "foo")
+	os.Setenv("APP_W", `bar`)
+
+	Expect(config.Register("simple", chunk)).To(BeNil())
+
+	errors := config.Load()
+	Expect(errors).To(HaveLen(2))
+	Expect(errors).To(ContainElement(MatchError("value supplied for field 'Y' cannot be coerced into the expected type")))
+	Expect(errors).To(ContainElement(MatchError("value supplied for field 'Z' cannot be coerced into the expected type")))
+}
+
+func (s *ConfigSuite) TestBadDefaultType(t *testing.T) {
+	var (
+		config = NewEnvConfig("app")
+		chunk  = &TestBadDefaultConfig{}
+	)
+
+	Expect(config.Register("bad-default", chunk)).To(BeNil())
+
+	errors := config.Load()
+	Expect(errors).To(HaveLen(1))
+	Expect(errors).To(ContainElement(MatchError("default value for field 'X' cannot be coerced into the expected type")))
+}
+
+func (s *ConfigSuite) TestToMap(t *testing.T) {
+	var (
+		config = NewEnvConfig("app")
+		chunk1 = &TestSimpleConfig{}
+		chunk2 = &TestEmbeddedJSONConfig{}
+	)
+
+	os.Setenv("APP_X", "foo")
+	os.Setenv("APP_Y", "123")
+	os.Setenv("APP_W", `["bar", "baz", "bonk"]`)
+	os.Setenv("APP_P1", `{"v_int": 3, "v_float": 3.14, "v_bool": true}`)
+	os.Setenv("APP_P2", `{"v_int": 5, "v_float": 6.28, "v_bool": false}`)
+
+	config.MustRegister("simple", chunk1)
+	config.MustRegister("json-embedded", chunk2)
+	config.Load()
+
+	dump, err := config.ToMap()
+	Expect(err).To(BeNil())
+
+	Expect(dump).To(HaveLen(5))
+	Expect(dump["X"]).To(Equal("foo"))
+	Expect(dump["Y"]).To(Equal("123"))
+	Expect(dump["Z"]).To(MatchJSON(`["bar", "baz", "bonk"]`))
+	Expect(dump["P1"]).To(MatchJSON(`{"v_int": 3, "v_float": 3.14, "v_bool": true}`))
+	Expect(dump["P2"]).To(MatchJSON(`{"v_int": 5, "v_float": 6.28, "v_bool": false}`))
+}
+
+func (s *ConfigSuite) TestMask(t *testing.T) {
+	var (
+		config = NewEnvConfig("app")
+		chunk  = &TestMaskConfig{}
+	)
+
+	os.Setenv("APP_X", "foo")
+	os.Setenv("APP_Y", "123")
+	os.Setenv("APP_W", `["bar", "baz", "bonk"]`)
+
+	config.MustRegister("masked", chunk)
+	config.Load()
+
+	dump, err := config.ToMap()
+	Expect(err).To(BeNil())
+
+	Expect(dump).To(HaveLen(1))
+	Expect(dump["X"]).To(Equal("foo"))
+}
+
+func (s *ConfigSuite) TestBadMaskTag(t *testing.T) {
+	var (
+		config = NewEnvConfig("app")
+		chunk  = &TestBadMaskTagConfig{}
+	)
+
+	config.MustRegister("bad-mask-tag", chunk)
+	config.Load()
+
+	_, err := config.ToMap()
+	Expect(err).To(MatchError("field 'X' has an invalid mask tag"))
+}
+
+func (s *ConfigSuite) TestPostLoadConfig(t *testing.T) {
+	var (
+		config = NewEnvConfig("app")
+		chunk  = &TestPostLoadConfig{}
+	)
+
+	Expect(config.Register("post-load", chunk)).To(BeNil())
+
+	os.Setenv("APP_X", "3")
+	Expect(config.Load()).To(BeEmpty())
+
+	os.Setenv("APP_X", "-4")
+	errors := config.Load()
+	Expect(errors).To(HaveLen(1))
+	Expect(errors).To(ContainElement(MatchError("X must be positive")))
+}
+
+func (s *ConfigSuite) TestUnsettableFieldS(t *testing.T) {
+	var (
+		config = NewEnvConfig("app")
+		chunk  = &TestUnsettableConfig{}
+	)
+
+	Expect(config.Register("unsettable", chunk)).To(BeNil())
+
+	errors := config.Load()
+	Expect(errors).To(HaveLen(1))
+	Expect(errors).To(ContainElement(MatchError("field 'x' can not be set")))
+}
+
+func (s *ConfigSuite) TestDuplicateRegistration(t *testing.T) {
+	var (
+		config = NewEnvConfig("app")
+		chunk  = &TestSimpleConfig{}
+	)
+
+	err1 := config.Register("dup", chunk)
+	err2 := config.Register("dup", chunk)
+	Expect(err1).To(BeNil())
+	Expect(err2).To(Equal(ErrDuplicateConfigKey))
+}
+
+func (s *ConfigSuite) TestMustRegisterPanics(t *testing.T) {
+	var (
+		config = NewEnvConfig("app")
+		chunk  = &TestSimpleConfig{}
+	)
+
+	config.MustRegister("dup", chunk)
+	Expect(config.Get("dup")).To(BeIdenticalTo(chunk))
+
+	Expect(func() {
+		config.MustRegister("dup", &TestSimpleConfig{})
+	}).To(Panic())
+}
+
+func (s *ConfigSuite) TestGetUnregisteredKey(t *testing.T) {
+	_, err := NewEnvConfig("app").Get("unregistered")
+	Expect(err).To(Equal(ErrUnregisteredKey))
+}
+
+func (s *ConfigSuite) TestMustGetPanics(t *testing.T) {
+	Expect(func() {
+		NewEnvConfig("app").MustGet("unregistered")
+	}).To(Panic())
+}
+
+//
+// Chunks
+
+type (
+	TestSimpleConfig struct {
+		X string   `env:"x"`
+		Y int      `env:"y"`
+		Z []string `env:"w"`
+	}
+
+	TestEmbeddedJSONConfig struct {
+		P1 *TestJSONPayload `env:"p1"`
+		P2 *TestJSONPayload `env:"p2"`
+	}
+
+	TestJSONPayload struct {
+		V1 int     `json:"v_int"`
+		V2 float64 `json:"v_float"`
+		V3 bool    `json:"v_bool"`
+	}
+
+	TestRequiredConfig struct {
+		X string `env:"x" required:"true"`
+	}
+
+	TestDefaultConfig struct {
+		X string   `env:"x" default:"foo"`
+		Y []string `env:"y" default:"[\"bar\", \"baz\", \"bonk\"]"`
+	}
+
+	TestBadDefaultConfig struct {
+		X int `env:"x" default:"foo"`
+	}
+
+	TestUnsettableConfig struct {
+		x int `env:"s"`
+	}
+
+	TestPostLoadConfig struct {
+		X int `env:"X"`
+	}
+
+	TestMaskConfig struct {
+		X string   `env:"x"`
+		Y int      `env:"y" mask:"true"`
+		Z []string `env:"w" mask:"true"`
+	}
+
+	TestBadMaskTagConfig struct {
+		X string `env:"x" mask:"34"`
+	}
+)
+
+func (c *TestPostLoadConfig) PostLoad() error {
+	if c.X < 0 {
+		return errors.New("X must be positive")
+	}
+
+	return nil
+}
