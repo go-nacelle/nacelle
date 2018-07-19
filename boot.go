52f0fbf15e417ff7b489a07724211db1536d037f
Eric Fritz <eric@eric-fritz.com>
2018-07-19 09:49:15 +0200

    Update boot process to remove config registration.

--- boot.go
+++ boot.go
@@ -3,6 +3,7 @@ package nacelle
 import (
 	"os"
 
+	"github.com/efritz/nacelle/config"
 	"github.com/efritz/nacelle/logging"
 	"github.com/efritz/nacelle/process"
 	"github.com/efritz/nacelle/service"
@@ -13,7 +14,6 @@ type (
 	Bootstrapper struct {
 		name              string
 		configs           map[interface{}]interface{}
-		configSetupFunc   ConfigSetupFunc
 		initFunc          AppInitFunc
 		loggingInitFunc   LoggingInitFunc
 		loggingFields     LogFields
@@ -26,27 +26,16 @@ type (
 		runnerConfigFuncs []RunnerConfigFunc
 	}
 
-	// ConfigSetupFunc is called by the bootstrap procedure to populate
-	// the config object with unpopulated config objects.
-	ConfigSetupFunc func(Config) error
-
 	// AppInitFunc is an program entrypoint called after performing initial
 	// configuration loading, sanity checks, and setting up loggers. This
 	// function should register initializers and processes and inject values
 	// into the service container where necessary.
 	AppInitFunc func(ProcessContainer, ServiceContainer) error
-
-	configToken string
-)
-
-var (
-	loggingConfigToken = configToken("nacelle-logging")
 )
 
 // NewBootstrapper creates an entrypoint to the program with the given configs.
 func NewBootstrapper(
 	name string,
-	configSetupFunc ConfigSetupFunc,
 	initFunc AppInitFunc,
 	bootstrapperConfigs ...BootstrapperConfigFunc,
 ) *Bootstrapper {
@@ -60,7 +49,6 @@ func NewBootstrapper(
 
 	return &Bootstrapper{
 		name:              name,
-		configSetupFunc:   configSetupFunc,
 		initFunc:          initFunc,
 		loggingInitFunc:   config.loggingInitFunc,
 		loggingFields:     config.loggingFields,
@@ -72,29 +60,9 @@ func NewBootstrapper(
 // method does not return in any meaningful way (it blocks until
 // the associated process runner has completed).
 func (bs *Bootstrapper) Boot() int {
-	serviceContainer, err := service.NewContainer()
-	if err != nil {
-		logging.LogEmergencyError("failed to create service container (%s)", err)
-		return 1
-	}
+	baseConfig := NewEnvConfig(bs.name)
 
-	config := NewEnvConfig(bs.name)
-	if err := config.Register(loggingConfigToken, &logging.Config{}); err != nil {
-		logging.LogEmergencyError("failed to register logging config (%s)", err)
-		return 1
-	}
-
-	if err := bs.configSetupFunc(config); err != nil {
-		logging.LogEmergencyError("failed to register configs (%s)", err)
-		return 1
-	}
-
-	if errs := config.Load(); len(errs) > 0 {
-		logging.LogEmergencyErrors("Failed to load configuration (%s)", errs)
-		return 1
-	}
-
-	logger, err := bs.loggingInitFunc(config)
+	logger, err := bs.loggingInitFunc(baseConfig)
 	if err != nil {
 		logging.LogEmergencyError("failed to initialize logging (%s)", err)
 		return 1
@@ -110,31 +78,26 @@ func (bs *Bootstrapper) Boot() int {
 
 	logger.Info("Logging initialized")
 
+	serviceContainer, err := service.NewContainer()
+	if err != nil {
+		logging.LogEmergencyError("failed to create service container (%s)", err)
+		return 1
+	}
+
 	if err := serviceContainer.Set("logger", logger); err != nil {
 		logger.Error("Failed to register logger to service container (%s)", err)
 		return 1
 	}
 
-	m, err := config.ToMap()
-	if err != nil {
-		logger.Error("Failed to serialize config (%s)", err.Error())
-		return 1
-	}
-
-	logger.InfoWithFields(m, "Config loaded from environment")
-
-	bs.runnerConfigFuncs = append(
-		bs.runnerConfigFuncs,
-		process.WithLogger(logger),
-	)
-
-	processContainer := process.NewContainer()
 	health := process.NewHealth()
 
 	if err := serviceContainer.Set("health", health); err != nil {
 		logger.Error("Failed to register health reporter to service container (%s)", err)
 		return 1
 	}
+
+	config := config.NewLoggingConfig(baseConfig, logger)
+	processContainer := process.NewContainer()
 
 	if err := bs.initFunc(processContainer, serviceContainer); err != nil {
 		logger.Error("Failed to run initialization function (%s)", err.Error())
@@ -145,7 +108,10 @@ func (bs *Bootstrapper) Boot() int {
 		processContainer,
 		serviceContainer,
 		health,
-		bs.runnerConfigFuncs...,
+		append(
+			bs.runnerConfigFuncs,
+			process.WithLogger(logger),
+		)...,
 	)
 
 	statusCode := 0
